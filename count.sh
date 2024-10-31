23a2d0f2d87ce7ff880e29cde24317e4eca7e418
Malte Weyrich <malte.weyrich@hotmail.de>
2024-10-31 19:20:48 +0100

    sh script for analyzing gtf and result files

--- count.sh
+++ count.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 for file in "$@"; do
     echo "$file"
-    cut -f9 "$file" | cut -f1 -d";" | sort | uniq | wc -l
+    # cut -f1 "$file" | sort | uniq | wc -l
+    grep -P "CDS\t" "$file" | cut -f9 | cut -f1 -d";" | sort | uniq | wc -l
+    # cat "$file" | wc -l
 done
