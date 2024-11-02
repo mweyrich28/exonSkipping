b11cce06d13f346113e03afaff3d0c02c740ecea
Malte Weyrich <malte.weyrich@hotmail.de>
2024-11-02 12:24:16 +0100

    cleaning up

--- src/main/java/org/src/utils/FileUtils.java
+++ src/main/java/org/src/utils/FileUtils.java
@@ -13,6 +13,7 @@ public class FileUtils{
 
         while((line = buff.readLine()) != null){
             // only read in lines with CDS / exon
+            // skip comments
             if (line.charAt(0) == '#') {
                 fileLines.add(line);
                 continue;
