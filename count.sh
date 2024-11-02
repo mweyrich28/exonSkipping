2c98a8b5573cca7def2c19eec5e63d24098da268
Malte Weyrich <malte.weyrich@hotmail.de>
2024-11-02 12:24:00 +0100

    additional count

--- count.sh
+++ count.sh
@@ -10,6 +10,15 @@ for file in "$@"; do
     # count total amoutn of ES-SE in results
     # tail -n +2 "$file" | wc -l
 
-    # check for occurences of ENSG00000155657.25 in all gtf files
-    cut -f9 "$file" | cut -f1 -d";" | grep -P "ENSG00000155657" | wc -l
+    # check for occurences of x in all gtf files
+    # cut -f9 "$file" | cut -f1 -d";" | grep -P "ENSG00000271425" | wc -l
+
+    # check max exons skipped of gene id x
+    x="ENSG00000283186"
+    echo "$x"
+    grep -P "$x" "$file" | cut -f12 | sort -r -n | head -n1
 done
+
+
+
+# 
